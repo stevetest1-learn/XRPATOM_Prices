4f06024c62cb0b3d338ee309b12a7697c2e83542
Steve Hatfield <stevenhatfield23@gmail.com>
2026-04-10 12:29:28 -0400

    updating and adding index to templates

--- web_app.py
+++ web_app.py
@@ -51,6 +51,6 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     debug = os.environ.get("FLASK_DEBUG", "1") == "1"
     app.run(debug=debug, host="0.0.0.0", port=port)
